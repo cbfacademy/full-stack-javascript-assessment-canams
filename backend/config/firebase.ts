d2f926935b1da47ffabeb9c62025b79ddab7d3ea
Chia <canams@hotmail.co.uk>
2024-01-21 22:47:47 +0000

    firebase config

--- backend/config/firebase.ts
+++ backend/config/firebase.ts
@@ -4,8 +4,10 @@ import { applicationDefault } from "firebase-admin/app"
 
 dotenv.config()
 
+const serviceAccount = JSON.parse(process.env.GOOGLE_CREDS)
+
 const firebase = admin.initializeApp({
-  credential: applicationDefault(),
+  credential: admin.credential.cert(serviceAccount),
 })
 
 export default {
